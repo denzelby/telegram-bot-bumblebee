17377160769ce7ba95577c529f4f1f9b742b8307
Andrei_Hubarevich <andrei_hubarevich@epam.com>
2015-11-14 00:26:23 +0300

    Added randomized selection of pictures. Selection prefers pictures with extensions.

--- telegram-bot-bumblebee-core/src/main/java/com/github/bumblebee/command/googlepics/GooglePicsApi.java
+++ telegram-bot-bumblebee-core/src/main/java/com/github/bumblebee/command/googlepics/GooglePicsApi.java
@@ -6,6 +6,6 @@ import feign.RequestLine;
 public interface GooglePicsApi {
 
     // https://developers.google.com/image-search/v1/jsondevguide
-    @RequestLine("GET /search/images?v=1.0&imgsz=medium|large&rsz=1&safe=active&q={query}")
+    @RequestLine("GET /search/images?v=1.0&imgsz=medium|large&rsz=8&safe=active&q={query}")
     GooglePicsResponse queryPictures(@Param("query") String search);
 }
