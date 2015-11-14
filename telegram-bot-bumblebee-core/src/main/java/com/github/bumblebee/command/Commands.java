1feb331d67c5cf208ee426126865c592d7a383b4
Andrei_Hubarevich <maildenzel@gmail.com>
2015-11-14 17:00:04 +0300

    Added Youtube search command. Added /start (usage info) command.

--- telegram-bot-bumblebee-core/src/main/java/com/github/bumblebee/command/Commands.java
+++ telegram-bot-bumblebee-core/src/main/java/com/github/bumblebee/command/Commands.java
@@ -1,27 +1,45 @@
 package com.github.bumblebee.command;
 
 import com.github.bumblebee.command.googlepics.GooglePictureSearchCommand;
+import com.github.bumblebee.command.start.StartCommand;
 import com.github.bumblebee.command.status.StatusCommand;
+import com.github.bumblebee.command.youtube.YoutubeSearchCommand;
+import com.github.bumblebee.service.RandomPhraseService;
 import telegram.api.BotApi;
 import telegram.api.FileApi;
 import telegram.polling.UpdateHandler;
 
+import java.util.Properties;
+
 public final class Commands {
 
+    private RandomPhraseService phraseService = new RandomPhraseService();
     private final BotApi botApi;
     private final FileApi fileApi;
+    private final Properties config;
 
-    public Commands(BotApi botApi, FileApi fileApi) {
+    public Commands(BotApi botApi, FileApi fileApi, Properties config) {
 
         this.botApi = botApi;
         this.fileApi = fileApi;
+        this.config = config;
     }
 
     public UpdateHandler statusCommand() {
         return new StatusCommand(botApi);
     }
 
+    public UpdateHandler startCommand() {
+        return new StartCommand(botApi);
+    }
+
     public UpdateHandler googlePictureSearchCommand() {
         return new GooglePictureSearchCommand(botApi);
+    }
+
+    public UpdateHandler youtubeSearchCommand() {
+        return new YoutubeSearchCommand(botApi, phraseService,
+                config.getProperty("youtube.api.projectname"),
+                config.getProperty("youtube.api.key"));
     }
 }
---
+++ telegram-bot-bumblebee-core/src/main/java/com/github/bumblebee/command/youtube/YoutubeSearchCommand.java
@@ -0,0 +1,97 @@
+package com.github.bumblebee.command.youtube;
+
+import com.github.bumblebee.command.SingleArgumentCommand;
+import com.github.bumblebee.service.RandomPhraseService;
+import com.google.api.client.http.apache.ApacheHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import telegram.api.BotApi;
+import telegram.domain.Update;
+
+import java.io.IOException;
+import java.util.List;
+
+public class YoutubeSearchCommand extends SingleArgumentCommand {
+
+    private static final Logger log = LoggerFactory.getLogger(YoutubeSearchCommand.class);
+
+    private static final String VIDEO_URL = "https://www.youtube.com/watch?v=";
+    private static final Long NUMBER_OF_VIDEOS_RETURNED = 1L;
+
+    private final BotApi botApi;
+    private final RandomPhraseService randomPhraseService;
+    private final YouTube youtube;
+    private final String googleApiKey;
+
+    public YoutubeSearchCommand(BotApi botApi, RandomPhraseService randomPhraseService, String googleAppName,
+                                String googleApiKey) {
+
+        checkGoogleApiCredentials(googleAppName, googleApiKey);
+
+        this.botApi = botApi;
+        this.randomPhraseService = randomPhraseService;
+        this.googleApiKey = googleApiKey;
+
+        this.youtube = new YouTube
+                .Builder(new ApacheHttpTransport(), new JacksonFactory(), req -> {})
+                .setApplicationName(googleAppName)
+                .build();
+    }
+
+    @Override
+    public void handleCommand(Update update, Integer chatId, String argument) {
+
+        if (argument == null) {
+            botApi.sendMessage(chatId, randomPhraseService.surprise());
+            return;
+        }
+
+        try {
+            String videoId = searchVideo(argument);
+            if (videoId != null) {
+                botApi.sendMessage(chatId, VIDEO_URL + videoId);
+                return;
+            }
+        } catch (IOException e) {
+            log.error("Error during youtube search", e);
+        }
+        botApi.sendMessage(chatId, randomPhraseService.no(), update.getMessage().getMessageId());
+    }
+
+    private String searchVideo(String searchQuery) throws IOException {
+
+        // Define the API request for retrieving search results.
+        YouTube.Search.List search = youtube.search().list("id");
+
+        search.setKey(googleApiKey);
+        search.setQ(searchQuery);
+
+        // Restrict the search results to only include videos.
+        search.setType("video");
+
+        // To increase efficiency, only retrieve the fields that the application uses.
+        search.setFields("items(id/videoId)");
+        search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
+
+        // Call the API and print results.
+        SearchListResponse searchResponse = search.execute();
+        List<SearchResult> searchResultList = searchResponse.getItems();
+        if (searchResultList != null && !searchResultList.isEmpty()) {
+            return searchResultList.get(0).getId().getVideoId();
+        }
+        return null;
+    }
+
+    private void checkGoogleApiCredentials(String googleAppName, String googleApiKey) {
+        if (googleAppName == null) {
+            throw new IllegalStateException("Google project name is not defined!");
+        }
+        if (googleApiKey == null) {
+            throw new IllegalStateException("Google api key is not defined!");
+        }
+    }
+}
