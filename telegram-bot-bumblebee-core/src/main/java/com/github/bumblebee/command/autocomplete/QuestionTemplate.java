109213de805f9426c7df0e47746b857173f5401a
ProxyBy <smartm@bk.ru>
2016-05-14 14:03:00 +0300

    Autocomplete command

---
+++ telegram-bot-bumblebee-core/src/main/java/com/github/bumblebee/command/autocomplete/QuestionTemplate.java
@@ -0,0 +1,9 @@
+package com.github.bumblebee.command.autocomplete;
+
+/**
+ * Created by Misha on 14.05.2016.
+ */
+public class QuestionTemplate {
+    public static final String DO_SVI_DOS = "ДО";
+    public static final String DOS_VI_DOS = "ДОС";
+}
