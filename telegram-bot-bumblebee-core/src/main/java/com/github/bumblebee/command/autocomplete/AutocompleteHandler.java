7a1b7e90958dab13880802e72332993a86bcc97e
ProxyBy <smartm@bk.ru>
2016-06-05 23:47:58 +0300

    Handler

--- telegram-bot-bumblebee-core/src/main/java/com/github/bumblebee/command/autocomplete/AutocompleteHandler.java
+++ telegram-bot-bumblebee-core/src/main/java/com/github/bumblebee/command/autocomplete/AutocompleteHandler.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Component;
 import telegram.api.BotApi;
 import telegram.domain.Update;
 
+import java.util.*;
+
 /**
  * Created by Misha on 14.05.2016.
  */
@@ -17,26 +19,36 @@ public class AutocompleteHandler extends ChainedMessageListener {
     private static final Logger log = LoggerFactory.getLogger(AutocompleteHandler.class);
 
     private final BotApi botApi;
+    private final AutocompleteConfig config;
+    private final Map<String,List<String>> autocomplete = new HashMap<String,List<String>>();
 
     @Autowired
-    public AutocompleteHandler(BotApi botApi) {
+    public AutocompleteHandler(BotApi botApi, AutocompleteConfig config) {
         this.botApi = botApi;
+        this.config = config;
+        config.getTemplate().forEach(this::parse);
+    }
+
+    private void parse(String phrase){
+        List<String> temp = new ArrayList<String>();
+        String key = null;
+
+        for (String part: phrase.split(" ")) {
+            if (key==null){
+                key = part;
+            } else {
+                temp.add(part);
+            }
+        }
+        autocomplete.put(key, temp);
     }
 
     @Override
     public boolean onMessage(Long chatId, String message, Update update) {
-
         try {
-            if (message.equalsIgnoreCase("ДО")) {
-                botApi.sendMessage(chatId, "СВИ");
+            if (autocomplete.containsKey(message)) {
+                autocomplete.get(message).forEach(phrase -> botApi.sendMessage(chatId, phrase));
                 Thread.sleep(1000);
-                botApi.sendMessage(chatId, "ДОС");
-                return true;
-            }
-            if (message.equalsIgnoreCase("ДОС")) {
-                botApi.sendMessage(chatId, "ВИ");
-                Thread.sleep(1000);
-                botApi.sendMessage(chatId, "ДОС");
                 return true;
             }
         } catch (InterruptedException e) {
