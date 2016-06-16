550e25d936488cf35a320ec2cd71800a2fb9ab33
dzianis.baburkin <dzianis.baburkin@office.axamit.com>
2016-06-16 17:39:19 +0300

    Weather refactoring

--- telegram-bot-bumblebee-core/src/main/java/com/github/bumblebee/command/weather/WeatherCommand.java
+++ telegram-bot-bumblebee-core/src/main/java/com/github/bumblebee/command/weather/WeatherCommand.java
@@ -1,5 +1,6 @@
 package com.github.bumblebee.command.weather;
 
+import com.github.bumblebee.command.SingleArgumentCommand;
 import com.github.bumblebee.utils.BumblebeeUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,7 +9,6 @@ import org.springframework.stereotype.Component;
 import telegram.api.BotApi;
 import telegram.domain.Update;
 import telegram.domain.request.InputFile;
-import telegram.polling.UpdateHandler;
 
 import java.io.IOException;
 import java.net.URL;
@@ -18,10 +18,25 @@ import java.net.URL;
  */
 
 @Component
-public class WeatherCommand implements UpdateHandler {
+public class WeatherCommand extends SingleArgumentCommand {
     private static final Logger log = LoggerFactory.getLogger(WeatherCommand.class);
     private final BotApi botApi;
     private final static String URL = "http://meteoinfo.by/radar/UMMN/radar-map.gif";
+    private final static String ERROR_MESSAGE = "ехай нахуй";
+
+    private enum WeatherArguments {
+        MAP("m"),
+        TEMPERATURE("t");
+        private final String argument;
+
+        WeatherArguments(String argument) {
+            this.argument = argument;
+        }
+
+        public String getArgument() {
+            return argument;
+        }
+    }
 
     @Autowired
     public WeatherCommand(BotApi botApi) {
@@ -29,14 +44,29 @@ public class WeatherCommand implements UpdateHandler {
     }
 
     @Override
-    public boolean onUpdate(Update update) {
+    public void handleCommand(Update update, Long chatId, String argument) {
+        switch (WeatherArguments.valueOf(argument)) {
+            case MAP:
+                getWeatherGraphic(chatId);
+                break;
+            case TEMPERATURE:
+                getWeatherTemperature(chatId);
+                break;
+            default:
+                botApi.sendMessage(chatId, ERROR_MESSAGE);
+        }
+    }
+
+    private void getWeatherGraphic(Long chatId) {
         try {
-            Long chatId = update.getMessage().getChat().getId();
             InputFile document = InputFile.document(new URL(URL).openStream(), BumblebeeUtils.getFileName(URL));
             botApi.sendDocument(chatId, document);
         } catch (IOException e) {
             log.error(e.getMessage(), e);
         }
-        return true;
+    }
+
+    private void getWeatherTemperature(Long chatId) {
+        botApi.sendMessage(chatId, "You are not prepared! (me)");
     }
 }
