fcd0c30926b26eae10a0cb821ec3d0146eec6e0e
fare1990 <farehome1990@gmail.com>
2016-06-16 13:08:50 +0300

    Added new command functionality (setting autocomplete templates) Added simple Security to new command executing

--- telegram-bot-bumblebee-core/src/main/java/com/github/bumblebee/command/autocomplete/AutocompleteAddCommand.java
+++ telegram-bot-bumblebee-core/src/main/java/com/github/bumblebee/command/autocomplete/AutocompleteAddCommand.java
@@ -2,6 +2,7 @@ package com.github.bumblebee.command.autocomplete;
 
 import com.github.bumblebee.command.SingleArgumentCommand;
 import com.github.bumblebee.security.BumblebeeSecurityConfig;
+import com.github.bumblebee.service.RandomPhraseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import telegram.api.BotApi;
@@ -14,16 +15,23 @@ public class AutocompleteAddCommand extends SingleArgumentCommand {
     private final BotApi botApi;
     private final AutocompleteHandler handler;
     private final BumblebeeSecurityConfig securityConfig;
+    protected final RandomPhraseService randomPhrase;
 
     @Autowired
-    public AutocompleteAddCommand(BotApi botApi, AutocompleteHandler handler, BumblebeeSecurityConfig securityConfig) {
+    public AutocompleteAddCommand(BotApi botApi, AutocompleteHandler handler, BumblebeeSecurityConfig securityConfig, RandomPhraseService randomPhrase) {
         this.botApi = botApi;
         this.handler = handler;
         this.securityConfig = securityConfig;
+        this.randomPhrase = randomPhrase;
     }
 
     @Override
     public void handleCommand(Update update, Long chatId, String argument) {
+
+        if (argument == null) {
+            botApi.sendMessage(chatId, randomPhrase.surprise());
+            return;
+        }
 
         //todo: implement proper security model
         if (!securityConfig.getAdminIds().contains(update.getMessage().getFrom().getId())) {
