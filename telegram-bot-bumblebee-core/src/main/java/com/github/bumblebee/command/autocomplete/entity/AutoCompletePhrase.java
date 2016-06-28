ae51b9fd403f494b67e5a47e9741c117bc69960a
fare1990 <farehome1990@gmail.com>
2016-06-28 11:16:57 +0300

    Added autocomplete saving to database functionality. (#29)
    * Added autocomplete saving to database functionality.
    
    * Added autocomplete saving to database functionality.
    
    * Added autocomplete saving to database functionality.
    
    * Added autocomplete saving to database functionality.

---
+++ telegram-bot-bumblebee-core/src/main/java/com/github/bumblebee/command/autocomplete/entity/AutoCompletePhrase.java
@@ -0,0 +1,44 @@
+package com.github.bumblebee.command.autocomplete.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "BB_AUTOCOMPLETE_PHRASES")
+public class AutoCompletePhrase {
+
+    @Id
+    private String phraseKey;
+
+    private String phrasePattern;
+
+    public AutoCompletePhrase() {
+
+    }
+
+    public AutoCompletePhrase(String phraseKey, String phrasePattern) {
+        this.phraseKey = phraseKey;
+        this.phrasePattern = phrasePattern;
+    }
+
+    public void setPhraseToComplete(String phraseKey) {
+        this.phraseKey = phraseKey;
+    }
+
+    public void setCompletePhrase(String phrasePattern) {
+        this.phrasePattern = phrasePattern;
+    }
+
+    public String getPhraseKey() {
+        return phraseKey;
+    }
+
+    public String getPhrasePattern() {
+        return phrasePattern;
+    }
+
+    @Override
+    public String toString() {
+        return "PhraseKey:" + phraseKey
+                + " PhrasePattern:" + phrasePattern;
+    }
+}
---
+++ telegram-bot-bumblebee-core/src/main/java/com/github/bumblebee/command/autocomplete/service/AutoCompleteService.java
@@ -0,0 +1,34 @@
+package com.github.bumblebee.command.autocomplete.service;
+
+import com.github.bumblebee.command.autocomplete.dao.AutoCompleteRepository;
+import com.github.bumblebee.command.autocomplete.entity.AutoCompletePhrase;
+import com.google.api.client.util.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class AutoCompleteService {
+
+    private AutoCompleteRepository repository;
+
+    @Autowired
+    public AutoCompleteService(AutoCompleteRepository repository) {
+        this.repository = repository;
+    }
+
+    public AutoCompletePhrase storeAutoCompletePhrase(AutoCompletePhrase phrase) {
+        return repository.save(phrase);
+    }
+
+    public void updateAutoCompletePhrase(String phraseKey, String phrasePattern) {
+        repository.updatePhrase(phrasePattern, phraseKey);
+    }
+
+    public List<AutoCompletePhrase> retrieveAutoCompletePhrases() {
+        return Lists.newArrayList(repository.findAll());
+    }
+}
