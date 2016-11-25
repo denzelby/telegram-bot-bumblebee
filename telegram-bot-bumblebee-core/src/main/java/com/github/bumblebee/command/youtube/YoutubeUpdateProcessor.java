708dc65eee566dbeffb0a1f47b3bc24c4a45d153
fare1990 <farehome1990@gmail.com>
2016-11-25 18:48:01 +0300

    Fix youtube push notifications video post mechanism. Posting same video is no more.

--- telegram-bot-bumblebee-core/src/main/java/com/github/bumblebee/command/youtube/YoutubeUpdateProcessor.java
+++ telegram-bot-bumblebee-core/src/main/java/com/github/bumblebee/command/youtube/YoutubeUpdateProcessor.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Component;
 import telegram.api.BotApi;
 
 import java.util.*;
-import java.util.concurrent.TimeUnit;
 
 @Component
 public class YoutubeUpdateProcessor {
--- telegram-bot-bumblebee-core/src/main/java/com/github/bumblebee/command/youtube/service/YoutubeSubscriptionService.java
+++ telegram-bot-bumblebee-core/src/main/java/com/github/bumblebee/command/youtube/service/YoutubeSubscriptionService.java
@@ -17,7 +17,6 @@ import org.springframework.stereotype.Service;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collector;
 
 
 @Service
