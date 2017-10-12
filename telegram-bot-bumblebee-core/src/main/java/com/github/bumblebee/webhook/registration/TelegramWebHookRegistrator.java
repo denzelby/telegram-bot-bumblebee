738d276f177fe4242b82f0d5e7a0ca943913674d
Andrei.Hubarevich <andrei.hubarevich@synesis.ru>
2017-10-12 13:32:34 +0300

    Fix setWebHook: use certificate on file system instead of classpath one.

--- telegram-bot-bumblebee-core/src/main/java/com/github/bumblebee/webhook/registration/TelegramWebHookRegistrator.java
+++ telegram-bot-bumblebee-core/src/main/java/com/github/bumblebee/webhook/registration/TelegramWebHookRegistrator.java
@@ -1,10 +1,12 @@
 package com.github.bumblebee.webhook.registration;
 
 import java.io.File;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import telegram.api.BotApi;
 import telegram.domain.SetWebHookResponse;
 import telegram.domain.request.InputFile;
@@ -12,40 +14,38 @@ import telegram.domain.request.InputFile;
 @Service
 public class TelegramWebHookRegistrator {
 
-  private static final Logger log = LoggerFactory.getLogger(TelegramWebHookRegistrator.class);
-  private static final String URL_POSTFIX = "/webhook";
+    private static final Logger log = LoggerFactory.getLogger(TelegramWebHookRegistrator.class);
+    private static final String URL_POSTFIX = "/webhook";
 
-  private final BotApi botApi;
+    private final BotApi botApi;
 
-  @Autowired
-  public TelegramWebHookRegistrator(BotApi botApi) {
-    this.botApi = botApi;
-  }
+    @Autowired
+    public TelegramWebHookRegistrator(BotApi botApi) {
+        this.botApi = botApi;
+    }
 
-  public boolean registerWebHook(String url, String filePath) {
-    log.info("Registering webhook: {}", url + URL_POSTFIX);
-    return setWebHook(url + URL_POSTFIX, filePath);
-  }
+    public boolean registerWebHook(String url, String certificatePath) {
+        log.info("Registering webhook: {}, certificate path: {}", url + URL_POSTFIX, certificatePath);
+        return setWebHook(url + URL_POSTFIX, certificatePath);
+    }
 
-  public boolean removeWebHook() {
-    log.info("Removing webhook...");
-    return setWebHook("", null);
-  }
+    public boolean removeWebHook() {
+        log.info("Removing webhook...");
+        return setWebHook("", null);
+    }
 
-  private boolean setWebHook(String hookUrl, String filePath) {
-    File file;
+    private boolean setWebHook(String hookUrl, String filePath) {
 
-    String resource = TelegramWebHookRegistrator.class.getClassLoader().getResource(filePath).getFile();
+        InputFile inputFile = null;
+        if (!StringUtils.isEmpty(filePath)) {
+            inputFile = InputFile.document(new File(filePath));
+        }
 
-    file = new File(resource);
+        SetWebHookResponse response = botApi.setWebhook(hookUrl, inputFile);
+        log.info("Webhook registration: success = {}, description = {}", response.getResult(),
+                response.getDescription());
 
-    InputFile inputFile = InputFile.document(file);
-
-    SetWebHookResponse response = botApi.setWebhook(hookUrl, inputFile);
-    log.info("Webhook registration: success = {}, description = {}", response.getResult(),
-        response.getDescription());
-
-    return response.getResult() != null && response.getResult();
-  }
+        return response.getResult() != null && response.getResult();
+    }
 
 }
