e37898d2fde3bbf52a985bbb871a1d7a8c99573c
Andrei.Hubarevich <andrei.hubarevich@synesis.ru>
2017-10-27 15:32:44 +0300

    Simplify reading of start.md file for /start command.

--- telegram-bot-bumblebee-core/src/main/java/com/github/bumblebee/command/start/StartCommand.kt
+++ telegram-bot-bumblebee-core/src/main/java/com/github/bumblebee/command/start/StartCommand.kt
@@ -1,26 +1,17 @@
 package com.github.bumblebee.command.start
 
-import org.apache.commons.io.IOUtils
+import org.apache.commons.io.FileUtils
+import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Component
 import telegram.api.BotApi
 import telegram.domain.Update
 import telegram.domain.request.ParseMode
 import telegram.polling.UpdateHandler
-import java.io.IOException
-import java.io.StringWriter
 
 @Component
 class StartCommand(val botApi: BotApi) : UpdateHandler {
     private val helpText: String by lazy {
-        try {
-            StartCommand::class.java.classLoader.getResourceAsStream("start.md").use { stream ->
-                val sw = StringWriter()
-                IOUtils.copy(stream, sw)
-                return@lazy sw.toString()
-            }
-        } catch (e: IOException) {
-            throw RuntimeException(e)
-        }
+        FileUtils.readFileToString(ClassPathResource("start.md").file)
     }
 
     override fun onUpdate(update: Update): Boolean {
